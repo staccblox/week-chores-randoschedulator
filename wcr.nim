63bff5a933f34ce454a6ba690ba1e9bd86d68efd
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-23 19:16:32 -0400

    Update wcr.nim

--- wcr.nim
+++ wcr.nim
@@ -1,1 +1,6 @@
-proc timesTwo(i: SomeNumber) = echo i * 2
+let chores: = ["vacuuming", "cleaning"]
+echo "how many times do you vacuum per week?"
+var vacuuming = readLine(stdin);
+echo "how many times do you clean per week?"
+var cleaning = readLine(stdin);
+echo "Depending on whether " + vacuuming + " or " + cleaning " is higher, balance out the likelihood of doing it by daily." 
