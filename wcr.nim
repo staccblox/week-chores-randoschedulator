389921ab6786f4deb31adf0e30405e6cbc3beaae
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-23 19:16:53 -0400

    Update wcr.nim

--- wcr.nim
+++ wcr.nim
@@ -3,4 +3,4 @@ echo "how many times do you vacuum per week?"
 var vacuuming = readLine(stdin);
 echo "how many times do you clean per week?"
 var cleaning = readLine(stdin);
-echo "Depending on whether " + vacuuming + " or " + cleaning " is higher, balance out the likelihood of doing it by daily." 
+echo "Depending on whether " + vacuuming + " or " + cleaning + " is higher, balance out the likelihood of doing it by daily." 
